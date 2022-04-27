ec598ac651105ad85e6436f8812212c8fb2b657c
Manny Dador <EADador@gmail.com>
2022-04-26 23:57:51 -0400

    MVP -win

--- mystery_word.py
+++ mystery_word.py
@@ -1,41 +1,45 @@
 import random
 
-def play_game(file):
-    with open(file, 'r') as f:
+def play_game():
+    with open('test-word.txt') as f:
         word_string = f.read()
-        # random_word = random.choice(word_string)
-        print(word_string)
-        
-
-    interested = input("Shall we play a game?\n")
-    # print(interested)
-
-    if interested.lower() == "y":
         word_list = word_string.split()
-        current_word = word_list[0]
+        current_word = random.choice(word_list)
+        # print(current_word)
+        
+    # Do you want to build a snowman?!    
+    interested = input("Shall we play a game? y/n \n")
+    if interested.lower() == "y":
         print("Ok.. let's play!\n" "Your word is", len(current_word), "letters long.")
     else:
         print("Ok.. no soup for you!!")
         exit()
 
-    
+# # The Game - Guesses while loop
+    answer = ' '
+    count = 8
+
+    while count > 0:
+        guess = input("\nPlease enter 1 letter to guess: ")
+        answer += guess
+
+        for letter in current_word:
+            if letter in answer:
+                print(letter, end=" ")
+
+            elif letter not in answer:
+                print("_", end=" ")
+        
+        if guess in current_word:
+            count -= 1
+            print("\nYou got one!\nYou have " + str(count) + " guesses remaining.")
+        elif guess not in current_word:
+            count -= 1
+            print("\nSorry no " + guess + "'s try again!\nYou have " + str(count) + " guesses remaining.")
+
+        elif count == 0:
+            print("Game Over!!\nNo more guesses left =(\nThe word was " + current_word)
 
 
 if __name__ == "__main__":
-    # Copied from word_frequency assignment
-    import argparse
-    from pathlib import Path
-
-    parser = argparse.ArgumentParser(
-        description='Get the word frequency in a text file.')
-    parser.add_argument('file', help='file to read')
-    args = parser.parse_args()
-
-    file = Path(args.file)
-    if file.is_file():
-        play_game(file)
-    else:
-        print(f"{file} does not exist!")
-        exit(1)
-
-    #play_game()
+    play_game()
