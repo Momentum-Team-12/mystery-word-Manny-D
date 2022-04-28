e1f93c3177f27f99a3cf40be57973fb66d18d140
Manny Dador <EADador@gmail.com>
2022-04-27 22:31:46 -0400

    Missing 1 2.0

--- mystery_word.py
+++ mystery_word.py
@@ -11,65 +11,64 @@ def play_game():
     interested = input("Greetings Professor Falken.\nShall we play a game? y/n \n")
     if interested.lower() == "y":
         print("Ok.. let's play!\n" "Your word is", len(current_word), "letters long.")
-    else:
+    elif interested.lower() == "n":
         print("A strange game.\nThe only winning move is not to play.\nHow about a nice game of chess?")
         exit()
+    else:
+        print("Invalid input. Try again.")
+        play_game()
+
 
 # The Game
     answer = []
-    #global count -> may need later
     count = 8
 
-    while count > 0:
-        guess = input("\nPlease enter 1 letter to guess: ")
-        answer += guess
+    while True:
         w = 0
+        guess = input("\nPlease enter 1 letter as your guess: ")
 
-        # Guess tracking
+        # Invalid input handling
+        # If input is a number:
         if not guess.isalpha(): # looks to be working now
-            print("Invalid entry. Please enter letters only.")
-            
+            print("\nInvalid entry: " + guess + ". Please enter a letter only.")
+        # If input is multiple characters:    
         elif len(guess) > 1:   # looks to be working even if correct letter entered
-            answer = ""
-            print("Invalid entry. Only 1 letter please.")
+            print("\nInvalid entry: " + guess + ". Only 1 letter please.")
 
+        # Guess logic
+        elif guess in answer: 
+            print("\nYou already tried that letter.\nYou have " + str(count) + " guesses remaining.")
         elif guess in current_word:
-            #count -= 1 # Removing as user shouldn't lose guesses if correct
             print("\nYou got one!\nYou have " + str(count) + " guesses remaining.")
-
-        elif guess not in current_word:
+            answer += guess
+        # elif guess not in current_word:
+        else:
             count -= 1
+            answer += guess
             print("\nSorry no " + guess + "'s try again!\nYou have " + str(count) + " guesses remaining.")
 
-
+    # Ends game when count is 0 - this logic looks off - but works
+        if count == 0:
+            print("\nGame Over!!\nNo more guesses left =(\nThe Mystery Word was " + current_word + ".")
+            break
+        
         # Loop: letters (char) vs _'s vs Win
         for char in current_word:
             if char in answer:
                 print(char, end = "")
-        
             #elif char not in answer:
             else:
                 print("_", end = "")
                 w += 1
 
-        if w == 0: # works but want to try something different
-            print("\nYou did it!\nThe Mystery Word was " + current_word + " and was solved in " + str(8 - count) + " guesses!")
+        if w == 0: # Working - tries removed though
+            print("\nYou did it!\nThe Mystery Word was " + current_word + "!")
             exit()
 
-        # elif guess in answer:
-        #     print("You already tried that letter.\nYou have " + str(count) + " guesses remaining.")
+        # if w == 0: # working but count is off / want to try something different
+        #     print("\nYou did it!\nThe Mystery Word was " + current_word + " and was solved in " + str(tries) + " guesses!")
+        #     exit()
 
-
-    if count == 0:
-        print("\nGame Over!!\nNo more guesses left =(\nThe word was " + current_word + ".")
-    #break
-
-
-
-    # Ends game when count is 0
-    # while count == 0:
-    #     print("\nGame Over!!\nNo more guesses left =(\nThe word was " + current_word + ".")
-    #     break
 
 if __name__ == "__main__":
     play_game()
